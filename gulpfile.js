0c988a47d79d0016178a82a457d6e3d5322bb90b
Monika Wieczorek <monika@nopio.com>
2017-09-08 11:58:03 +0200

    styles for bacgkround video

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,8 @@ var pkg = require('./package.json');
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
-
-    gulp.src(['node_modules/tether/dist/js/*.js'])
-        .pipe(gulp.dest('vendor/tether'))
 })
 
 // Default task
